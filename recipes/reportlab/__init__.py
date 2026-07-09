3bb5b8b8ef4916f3388ec781d0dd10f0dcc213bd
mukeshtak111 <mukeshtak111@gmail.com>
2026-07-09 22:04:25 +0530

    Update __init__.py

--- recipes/reportlab/__init__.py
+++ recipes/reportlab/__init__.py
@@ -1,8 +1,11 @@
 from pythonforandroid.recipe import PythonRecipe
 
 class ReportLabRecipe(PythonRecipe):
-    version = '4.0.0'
-    url = 'https://files.pythonhosted.org/packages/source/r/reportlab/reportlab-{version}.tar.gz'
-    depends = ['setuptools']
+    version = "4.4.2"
+    url = "https://files.pythonhosted.org/packages/source/r/reportlab/reportlab-{version}.tar.gz"
+
+    depends = ["python3", "setuptools"]
+    
+    call_hostpython_via_targetpython = False
 
 recipe = ReportLabRecipe()
